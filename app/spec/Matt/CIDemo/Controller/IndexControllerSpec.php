eead00bce3f13449080cdef47b3fc449921318d9
Matt Cavanagh <matt.cavanagh@dealtrak.co.uk>
2019-09-26 11:59:37 +0100

    Expanding the tests further, including html and json bodies

--- app/spec/Matt/CIDemo/Controller/IndexControllerSpec.php
+++ app/spec/Matt/CIDemo/Controller/IndexControllerSpec.php
@@ -8,13 +8,19 @@ use Matt\CIDemo\Service\JsonResponseService;
 use PhpSpec\ObjectBehavior;
 use Zend\Diactoros\Request;
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 
 class IndexControllerSpec extends ObjectBehavior
 {
+    private $htmlBody = 'foo';
+    private $jsonBody = ['foo' => 'bar'];
+
     function let(
         HtmlResponseService $htmlResponseService,
         JsonResponseService $jsonResponseService
     ) {
+        $htmlResponseService->generateResponse($this->htmlBody)->willReturn(new HtmlResponse($this->htmlBody));
+        $jsonResponseService->generateResponse($this->jsonBody)->willReturn(new JsonResponse($this->jsonBody));
         $this->beConstructedWith($htmlResponseService, $jsonResponseService);
     }
 
@@ -23,9 +29,15 @@ class IndexControllerSpec extends ObjectBehavior
         $this->shouldHaveType(IndexController::class);
     }
 
-    function it_should_return_html_response(Request $request)
+    function it_should_return_successful_html_response_instance(Request $request)
     {
         $this->index($request)
             ->shouldReturnAnInstanceOf(HtmlResponse::class);
+    }
+
+    function it_should_return_successful_json_response_instance(Request $request)
+    {
+        $this->jsonIndex($request)
+            ->shouldReturnAnInstanceOf(JsonResponse::class);
     }
 }
